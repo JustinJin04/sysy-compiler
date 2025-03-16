77750269db5a2bdb82c493a23dbc12a1c6bfa6eb
Justin <justin.jin@stu.pku.edu.cn>
2025-03-16 13:33:38 +0000

    yet another try to fix pruning bugs

--- src/frontend/prune.hpp
+++ src/frontend/prune.hpp
@@ -5,7 +5,10 @@ namespace AST {
 
 class PruningRetVisitor: public Visitor{
  public:
-  bool pruning_end = false;
+  // bool pruning_end = false;
+  bool pruning_ret = false;
+  // bool pruning_break = false;
+  // bool pruning_continue = false;
 
   /**
    * pruning the AST to remove all stmt that is
@@ -13,46 +16,40 @@ class PruningRetVisitor: public Visitor{
    */
 
   void visit(ConstDecl& constdecl) override {
-    assert(pruning_end == false);
     if(constdecl.next_block_item) {
       constdecl.next_block_item->accept(*this);
     }
   }
 
   void visit(VarDecl& vardecl) override {
-    assert(pruning_end == false);
     if(vardecl.next_block_item) {
       vardecl.next_block_item->accept(*this);
     }
   }
 
   void visit(RetStmt& retstmt) override {
-    assert(pruning_end == false);
     std::cout<<"pruning retstmt"<<std::endl;
     retstmt.next_block_item.reset(nullptr);
-    pruning_end = true;
+    pruning_ret = true;
   }
 
   void visit(AssignStmt& assignstmt) override {
-    assert(pruning_end == false);
     if(assignstmt.next_block_item) {
       assignstmt.next_block_item->accept(*this);
     }
   }
 
   void visit(ExpStmt& expstmt) override {
-    assert(pruning_end == false);
     if(expstmt.next_block_item) {
       expstmt.next_block_item->accept(*this);
     }
   }
 
   void visit(BlockStmt& blockstmt) override {
-    assert(pruning_end == false);
     if(blockstmt.block_item){
       blockstmt.block_item->accept(*this);
     }
-    if(pruning_end) {
+    if(pruning_ret) {
       blockstmt.next_block_item.reset(nullptr);
     } else {
       if(blockstmt.next_block_item) {
@@ -63,56 +60,49 @@ class PruningRetVisitor: public Visitor{
 
   void visit(IfStmt& ifstmt) override {
     // std::cout<<"pruning ifstmt"<<std::endl;
-    assert(pruning_end == false);
+    assert(pruning_ret == false);
     if(ifstmt.then_body){
       std::cout<<"pruning then"<<std::endl;
       ifstmt.then_body->accept(*this);
     }
     // assert(pruning_end == false);
-    pruning_end = false;
+    pruning_ret = false;
     if(ifstmt.else_body){
       std::cout<<"pruning else"<<std::endl;
       ifstmt.else_body->accept(*this);
     }
     // assert(pruning_end == false);
-    pruning_end = false;
+    pruning_ret = false;
     if(ifstmt.next_block_item) {
       ifstmt.next_block_item->accept(*this);
     }
   }
 
   void visit(WhileStmt& whilestmt) override {
-    assert(pruning_end == false);
     if(whilestmt.body) {
       whilestmt.body->accept(*this);
     }
-    // if(pruning_end) {
-    //   whilestmt.next_block_item.reset(nullptr);
-    // } else {
-    //   if(whilestmt.next_block_item) {
-    //     whilestmt.next_block_item->accept(*this);
-    //   }
-    // }
-    
-    pruning_end = false;
-    if(whilestmt.next_block_item) {
-      whilestmt.next_block_item->accept(*this);
+    if(pruning_ret) {
+      whilestmt.next_block_item.reset(nullptr);
+    } else {
+      if(whilestmt.next_block_item) {
+        whilestmt.next_block_item->accept(*this);
+      }
     }
+    
+    // pruning_end = false;
+    // if(whilestmt.next_block_item) {
+    //   whilestmt.next_block_item->accept(*this);
+    // }
   }
 
   void visit(BreakStmt& breakstmt) override {
-    assert(pruning_end == false);
     breakstmt.next_block_item.reset(nullptr);
-    pruning_end = true;
   }
 
   void visit(ContinueStmt& continuestmt) override {
-    assert(pruning_end == false);
     continuestmt.next_block_item.reset(nullptr);
-    pruning_end = true;
   };
-
-
 
 };
 
